e4692a02e7beb261cc2478e411311a0fcff37cbb
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-15 15:42:36 -0300

    update

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 import styles from './styles';
 import {useRoute, RouteProp} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -20,7 +20,44 @@ export default function Order() {
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
         <TouchableOpacity>
-        <Icon name={'trash-can'} size={40} color={'#ffffff'} />
+        <Icon name={'trash-can'} size={40} color={'#FF3F4B'} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{color: '#FFFFFF'}}>
+          Pizzas
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{color: '#FFFFFF'}}>
+          Pizza de calabresa
+        </Text>
+      </TouchableOpacity>
+
+      <View style={styles.qtdContainer}>
+        <Text style={styles.qtdText}>
+          Quantidade
+        </Text>
+        <TextInput
+        style={[styles.input, { width: '60%', textAlign: 'center'}]}
+        placeholder='1'
+        placeholderTextColor={'#FFFFFF'}
+        keyboardType='numeric'
+        value='1'
+        />
+      </View>
+      
+      <View style={styles.actions}>
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Icon name={"plus"} size={30} color={'#FFFFFF'}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>
+            Avançar
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
--- src/screens/Order/styles.tsx
+++ src/screens/Order/styles.tsx
@@ -9,37 +9,65 @@ const styles = StyleSheet.create({
     paddingEnd: '4%',
     paddingStart: '4%'
   },
-  image: {
-    width: 250,
-    height: 240,
-    margin: '5%',
-  },
-  textInput: {
-    width: '90%',
-    backgroundColor: '#101026',
-    color: '#fff',
-    margin: 10,
-    paddingHorizontal: 8,
-  },
-  button: {
-    width: '80%',
-    backgroundColor: '#00FF7F',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    margin: 10,
-  },
-  contentInput: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 20,
-  },
-  textButton: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#1D1D2E',
-  },
+ header:{
+  flexDirection: 'row',
+  marginBottom: 12,
+  alignItems: 'center',
+  marginTop: 24
+ },
+ title:{
+  fontSize: 30,
+  fontWeight: 'bold',
+  color: '#FFFFFF',
+  marginRight: 14
+ }, 
+ input:{
+  backgroundColor: '#101026',
+  borderRadius: 4,
+  width: '100%',
+  height: 50,
+  marginBottom: 12,
+  justifyContent: 'center',
+  paddingHorizontal: 8,
+  color: '#FFFFFF',
+  fontSize: 20
+ },
+ qtdContainer:{
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+ },
+ qtdText:{
+  fontSize: 20,
+  fontWeight: 'bold',
+  color: '#FFFFFF'
+ },
+ actions:{
+  flexDirection: 'row',
+  width: '100%',
+  justifyContent: 'space-between'
+ },
+ buttonAdd:{
+  backgroundColor: '#3FD1FF',
+  borderRadius: 4,
+  height: 50,
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '25%'
+ },
+ button:{
+  backgroundColor: '#3FFFA3',
+  height: 50,
+  width: '70%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 4
+ }, 
+ textButton:{
+  color: '#101026',
+  fontSize: 26,
+  fontWeight: 'bold'
+ }
 });
 
 export default styles;
